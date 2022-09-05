ee917b3a3a90a5835b6bfc5f82f506f4d3299a2f
m.poettgen@gmx.net <82958985+poettas@users.noreply.github.com>
2022-09-05 15:05:19 +0200

    added userController & routes to project

--- controller/userController.js
+++ controller/userController.js
@@ -1,32 +1,24 @@
-
-import { UserDataModel } from "../schemas/user/userSchema";
-import { authenticateToken, createAccessToken } from "../security/jwt_auth";
-
+import { UserDataModel } from '../schemas/user/userSchema';
+import { authenticateToken, createAccessToken } from '../security/jwt_auth';
 
 export const register = async (req, res) => {
     try {
-        let dataOfUser = {}
-        const hashPW = await bcrypt.hash(req.body.password, saltRounds)
+        let dataOfUser = {};
+        const hashPW = await bcrypt.hash(req.body.password, saltRounds);
 
-        dataOfUser = {
+        (dataOfUser = {
             mail: req.body.email,
             firstname: req.body.firstname,
             lastname: req.body.lastname,
             username: req.body.username,
             password: hashPW,
             group: userGroups[0],
-            movies: 0,
-            music: 0,
-            images: 0,
-            storage: 400000,
-        },
-
-        //SAVE: userData to userDB
-        UserDataModel(dataOfUser).save()
-        res.send('Successfull registrated!')
-
+        }),
+            //SAVE: userData to userDB
+            UserDataModel(dataOfUser).save();
+        res.send('Successfull registrated!');
     } catch (error) {
-        console.log("ERROR:", error, "Error by registration!")
+        console.log('ERROR:', error, 'Error by registration!');
     }
 };
 
@@ -34,28 +26,36 @@ export const register = async (req, res) => {
 // PASSWORD: LoginPW123!
 
 exports.login = async (req, res) => {
-    console.log("Login process started... ")
+    console.log('Login process started... ');
 
     //Find: userData in userDB
-    const userFromDB = await UserDataModel.findOne({ username: req.body.username })
+    const userFromDB = await UserDataModel.findOne({
+        username: req.body.username,
+    });
     try {
         // COMPARE: loginData === userData
-        const isLogedIn = await bcrypt.compare(req.body.password, userFromDB.password)
+        const isLogedIn = await bcrypt.compare(
+            req.body.password,
+            userFromDB.password
+        );
 
-        if (isLogedIn === false) return
+        if (isLogedIn === false) return;
 
         const userData = {
             userID: userFromDB._id,
             username: userFromDB.username,
             usergroup: userFromDB.group,
-        }
+        };
 
-        const generateToken = createAccessToken(userData)
+        const generateToken = createAccessToken(userData);
 
         // Send Data to Frontend
-        res.send({ isLogedIn: isLogedIn, generateToken: generateToken, userData })
-
+        res.send({
+            isLogedIn: isLogedIn,
+            generateToken: generateToken,
+            userData,
+        });
     } catch (error) {
-        console.log("ERROR:", "Error by Login!", error)
+        console.log('ERROR:', 'Error by Login!', error);
     }
 };
