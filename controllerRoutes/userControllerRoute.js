f3c968ae4c56d019513516e1ef031607aaf8d33e
Marc Poettgen <82958985+poettas@users.noreply.github.com>
2022-09-08 09:13:29 +0200

    Marc setup express (#23)
    * added mongoconnect
    
    * added userController & routes to project
    
    * rework process on merged files
    
    * worked on upcoming errors after merge
    
    * added dummy text to routes
    
    * rename controllerRoute to registerRoute
    
    * changed something in app.js
    
    * added dummy text to routes

--- controllerRoutes/userControllerRoute.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import router from express.Router();
-import { register, login } from ("../controller/userController");
-
-export const registerRouter = router.post('/register', register)
-export const loginRouter = router.post("/login", login)
--- schemas/article/components/powerAdapterSchema.js
+++ schemas/article/components/powerAdapterSchema.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose'
-import { basicArticleData } from '../basicData&Settings/basicArticleData'
-import { schemaSettings } from '../basicData&Settings/schemaSettings'
+import mongoose from 'mongoose';
+import { basicArticleData } from '../basicData&Settings/basicArticleData.js';
+import { schemaSettings } from '../basicData&Settings/schemaSettings.js';
 
 export const powerAdapterSchema = mongoose.Schema({
     baseData: basicArticleData,
@@ -10,8 +10,8 @@ export const powerAdapterSchema = mongoose.Schema({
     cache: schemaSettings.number,
     maxPowerConsumption: schemaSettings.number,
     shockResistance: schemaSettings.number,
-})
+});
 export const ArticlePowerAdapterModel = mongoose.model(
     'powerAdapterCol',
     powerAdapterSchema
-)
+);
