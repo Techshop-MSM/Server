eb4b82bbefbd081d63a0aa7e5ad9c7150c7afed8
Martin <martinkr90@googlemail.com>
2022-09-12 17:59:48 +0200

    registration und Login gefixed

--- controllerRoutes/userRegisterRoute.js
+++ controllerRoutes/userRegisterRoute.js
@@ -1,33 +1,27 @@
-import { UserDataModel } from "../schemas/user/userSchema.js";
-import bcrypt from "bcrypt"
+import { UserDataModel } from '../schemas/user/userSchema.js';
+import bcrypt from 'bcrypt';
 
 const register = async (req, res) => {
+    const saltRounds = 10;
 
-    const saltRounds = 10
-    console.log(req.body)
     try {
-        let dataOfUser = {}
-        const hashedRegisterPassword = await bcrypt.hash(req.body.password, saltRounds)
+        let dataOfUser = {};
+        const hashedRegisterPassword = await bcrypt.hash(
+            req.body.password,
+            saltRounds
+        );
 
-        dataOfUser = {
+        (dataOfUser = {
             mail: req.body.email,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
             username: req.body.username,
             password: hashedRegisterPassword,
-            group: req.body.userGroup,
-            movies: 0,
-            music: 0,
-            images: 0,
-            storage: 400000,
-        },
-
-        //SAVE: userData to userDB
-        UserDataModel(dataOfUser).save()
-        res.send('Successfull registrated!')
-
+            group: req.body.userGroupe,
+        }),
+            //SAVE: userData to userDB
+            UserDataModel(dataOfUser).save();
+        res.send('Successfull registrated!');
     } catch (error) {
-        console.log("ERROR:", error, "Error by registration!")
+        console.log('ERROR:', error, 'Error by registration!');
     }
 };
 
